8a1a0b60d66a36670256254f432fad1639a4079c
svolodarskyi <sergii.volodarskyi@gmail.com>
2025-06-26 04:42:18 -0600

    init

---
+++ src/routers/index.js
@@ -0,0 +1,10 @@
+// src/routers/index.js
+
+import { Router } from 'express';
+//import routers
+
+const router = Router();
+
+//add here router.use();
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,47 @@
+// src/server.js
+
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import router from './routers/index.js';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
+    }),
+  );
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.use(router);
+
+  app.use(notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
