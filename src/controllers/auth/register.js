dd9439c3890904db75df90cd7c84afddc9cef3eb
Sergii Volodarskyi <47503455+svolodarskyi@users.noreply.github.com>
2025-06-28 15:07:11 -0600

    Create register.js

---
+++ src/controllers/auth/register.js
@@ -0,0 +1,15 @@
+import { registerUser } from '../../services/auth/user.js';
+
+export const registerUserController = async (req, res) => {
+  const user = await registerUser(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully registered a user!',
+    data: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+  });
+};
