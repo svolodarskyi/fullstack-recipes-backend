d8ad544a1b98c3806e8b7b418895db2819276a0a
Sergii Volodarskyi <47503455+svolodarskyi@users.noreply.github.com>
2025-06-28 15:07:47 -0600

    Update register.js

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -1,4 +1,4 @@
-import { registerUser } from '../../services/auth/user.js';
+import { registerUser } from '../../services/auth/user.auth.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
