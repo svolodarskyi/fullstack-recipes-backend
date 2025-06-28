32a9a07b62011e93b2cbb5ad9d9272242813c883
svolodarskyi <sergii.volodarskyi@gmail.com>
2025-06-28 16:11:38 -0600

    add swagger

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,5 @@
+import path from 'path';
+
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
