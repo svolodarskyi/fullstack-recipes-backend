0b0bcae739bc87d27b8cdc0d0f592acd8fb764dc
Sergii Volodarskyi <47503455+svolodarskyi@users.noreply.github.com>
2025-07-05 20:52:54 -0600

    Update isValidId.js

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -4,8 +4,8 @@ import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
 
 export const isValidId = (req, res, next) => {
-  const { contactId } = req.params;
-  if (!isValidObjectId(contactId)) {
+  const { recipeId } = req.params;
+  if (!isValidObjectId(recipeId)) {
     throw createHttpError(404, 'Contact not found');
   }
 
